cbefbf08e93a13bfdbbd8f6f57c3e0ac8012ca12
ishanVaghasiya <ishanvaghasiya2786@gmail.com>
2024-11-30 20:03:44 +0530

    Remove unwanted code

--- src/modules/kanban/KanbanBoard.tsx
+++ src/modules/kanban/KanbanBoard.tsx
@@ -4,26 +4,9 @@ import KBBoard from "./components/KBBoard";
 interface KanbanBoardProps {}
 
 const KanbanBoard: FC<KanbanBoardProps> = () => {
-  const [message, setMessage] = useState<string | null>(null);
-
-
-  const sendMessage = async () => {
-    const res = await fetch("/api/pusher", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ message: "Hello from Pusher!" }),
-    });
-
-    const data = await res.json();
-    console.log(data);
-  };
-
+  
   return (
     <div>
-      <h1>Pusher with Next.js</h1>
-      <button onClick={sendMessage}>Send Pusher Event</button>
-      {message && <div>Received Message: {message}</div>}
-
       <KBBoard />
     </div>
   );
