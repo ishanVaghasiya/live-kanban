2cd48b97db0df59969fff2818df62c37a86476e8
ishanVaghasiya <ishanvaghasiya2786@gmail.com>
2024-11-30 19:25:15 +0530

    Wrap page with protector router

--- pages/board.tsx
+++ pages/board.tsx
@@ -1,20 +1,29 @@
+// import MasterLayout from "@layouts";
+// import ProtectedRoute from "@layouts";
+// import {ProtectedRoute, MasterLayout} from ";
 import KanbanBoard from "@modules/kanban/KanbanBoard";
 import { Box, Container } from "@mui/material";
+import MasterLayout from "pageLayouts/MasterLayout";
+import ProtectedRoute from "pageLayouts/ProtectedLayout";
 
 export default function Home() {
   return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <KanbanBoard />
-      </Box>
-    </Container>
+    <ProtectedRoute>
+      <MasterLayout>
+        <Container maxWidth="lg">
+          <Box
+            sx={{
+              my: 4,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <KanbanBoard />
+          </Box>
+        </Container>
+      </MasterLayout>
+    </ProtectedRoute>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,65 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '../src/Link';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import { useAppSelector } from "@redux/hooks/hook";
+import { logout } from "@redux/slice/authslice";
+import { persistor } from "@redux/store";
+import * as React from "react";
+import { useDispatch } from "react-redux";
+import Copyright from "../src/Copyright";
+import Link from "../src/Link";
+import ProTip from "../src/ProTip";
+import ProtectedRoute from "pageLayouts/ProtectedLayout";
+import MasterLayout from "pageLayouts/MasterLayout";
 
 export default function Home() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const dispatch = useDispatch();
+
+  const userEmail = useAppSelector((state) => state?.auth?.user?.email);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    // Dispatch logout action to clear Redux state
+    dispatch(logout());
+
+    // Clear persisted Redux state
+    persistor.purge().then(() => {
+      console.log("Redux state cleared on logout!");
+    });
+  };
+
   return (
-    <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI - Next.js example in TypeScript
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <ProtectedRoute>
+      <MasterLayout>
+        <Container maxWidth="lg">
+          <Box
+            sx={{
+              my: 4,
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
+              Material UI - Next.js example in TypeScript
+            </Typography>
+            <Link href="/about" color="secondary">
+              Go to the about page
+            </Link>
+            <ProTip />
+            <Copyright />
+          </Box>
+        </Container>
+      </MasterLayout>
+    </ProtectedRoute>
   );
 }
